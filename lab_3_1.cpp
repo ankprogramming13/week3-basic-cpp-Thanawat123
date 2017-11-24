ce925e54d429cdd4edddec2618554cdf8460c170
Unknown <zidsod.6840@gmail.com>
2017-11-24 13:57:28 +0700

    ข้อที่1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Ms." << endl;
+  cout << "Thanawat." << endl;
+  cout << "Sangsawang." << endl;
+  cout << "Nok." << endl;
+  cout << "Male." << endl;
+  cout << "07/09/2541." << endl;
+  cout << "Play badminton." << endl;
+  cout << "Single." << endl;
   return 0;
 }
